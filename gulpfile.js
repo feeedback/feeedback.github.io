3e96bc35d06bd81051c292468e8955da8d5c8dee
A. Rzhanitsyn <risdeep@gmail.com>
2020-08-20 02:07:59 +0500

    ADD gulp postcss ecosystem, add gulp paths variable

--- gulpfile.js
+++ gulpfile.js
@@ -1,96 +1,82 @@
 const { watch, series, parallel, dest, src } = require('gulp');
 const { execSync } = require('child_process');
 const bs = require('browser-sync').create();
-// const sass = require('gulp-sass');
-const cleanCSS = require('gulp-clean-css');
 const htmlmin = require('gulp-htmlmin');
 const terser = require('gulp-terser');
 // const gulpPrintSize = require('gulp-size');
-const webp = require('gulp-webp');
-// const fs = require('fs').promises;
-// const glob = require('glob');
 // const del = require('del');
 
+const webp = require('gulp-webp');
 const imageResize = require('gulp-image-resize');
 const rename = require('gulp-rename');
 const imagemin = require('gulp-imagemin');
 const pngToJpeg = require('png-to-jpeg');
-// const path = {
-//     html: ['*.html', '_includes/*.html', '_layouts/*.html'],
-//     scss: ['scss/**/*.scss'],
-//     // css: ['assets/css/*.css'],
-//     // posts: ['_posts/*.*', '_country-list/*.*'],
-//     config: ['_config.yml', '_data/*'],
-// };
-// const allPaths = Object.values(path).flat();
 
-// gulp.task('sass', function() {
-//     return gulp
-//         .src('scss/main.scss')
-//         .pipe(sass().on('error', sass.logError))
-//         .pipe(gulp.dest('_site/assets/styles/'))
-//         .pipe(bs.stream())
-//         .pipe(gulp.dest('assets/styles/'));
-// });
+const gulpPostcss = require('gulp-postcss');
+const autoprefixer = require('autoprefixer');
+const cssDeclarationSorter = require('css-declaration-sorter');
+const cssnano = require('cssnano');
+const postcssFlexbugsFixes = require('postcss-flexbugs-fixes');
+const postcssCustomProperties = require('postcss-custom-properties');
+const postcssCalc = require('postcss-calc');
+const postcssMergeRules = require('postcss-merge-rules');
+const postcssScssParser = require('postcss-scss');
+
+const paths = {
+    imgRawData: '_data/img_src',
+    imgSrc: 'assets/img',
+    cssSrc: 'assets/css',
+    build: '_site',
+};
 
 // const forWidthAvatar = [320, 360, 375, 411, 543, 580, 768, 992, 1200];
 const resizeAvatar = (cb) => {
     [203, 236, 249, 280, 372, 303, 175, 255, 315].forEach((size) => {
-        src([
-            '_data/img_src/**/avatar.{jpg,jpeg,png}',
-            '_data/img_src/**/!*_fallback.{jpg,jpeg,png}',
-        ])
+        src(`${paths.imgRawData}/**/avatar.{jpg,jpeg,png}`)
             .pipe(imageResize({ width: size }))
             .pipe(rename({ suffix: `_${size}w` }))
-            .pipe(dest('_data/img_src'));
+            .pipe(dest(paths.imgRawData));
     });
     cb();
 };
-
 // const forWidthProject = [320, 360, 375, 411, 580, 768, 992, 1200];
 const resizeProjectImage = (cb) => {
     // for [320w, 360w, 375w, 411w, 580w+, 768w+, 2X 992w+, 3X 1200w+]
     [272, 312, 327, 363, 492, 672, 445, 349].forEach((size) => {
         src([
-            '_data/img_src/projects/project-*+([^0-9][^w]).{jpg,jpeg,png}',
-            '_data/img_src/**/!*_fallback.{jpg,jpeg,png}',
+            `${paths.imgRawData}/**/project-*+([^0-9][^w]).{jpg,jpeg,png}`,
+            `${paths.imgRawData}/**/!*_fallback.{jpg,jpeg,png}`,
         ])
             .pipe(imageResize({ width: size }))
             .pipe(rename({ suffix: `_${size}w` }))
-            .pipe(dest('_data/img_src/projects'));
+            .pipe(dest(paths.imgRawData));
     });
     cb();
 };
-const resizeImage = series(resizeAvatar, resizeProjectImage);
+const resizeImage = parallel(resizeAvatar, resizeProjectImage);
 
 const minifyImage = (cb) => {
     src([
-        '_data/img_src/**/*_*.{jpg,jpeg,png}',
-        '_data/img_src/**/!*_fallback.{jpg,jpeg,png}',
+        `${paths.imgRawData}/**/*_*.{jpg,jpeg,png}`,
+        `${paths.imgRawData}/**/!*_fallback.{jpg,jpeg,png}`,
     ])
         .pipe(webp({ quality: 92, method: 6 }))
-        .pipe(dest('assets/img'));
+        .pipe(dest(paths.imgSrc));
     cb();
 };
+
 const minifyImageJPGForFallback = (cb) => {
     const defaultWidth = 312;
-    src(
-        [
-            '_data/img_src/**/avatar.{jpg,jpeg,png}',
-            '_data/img_src/**/project-*+([^0-9][^w]).{jpg,jpeg,png}',
-            '_data/img_src/**/!*_fallback.{jpg,jpeg,png}',
-        ]
-        //
-    )
+    src([
+        `${paths.imgRawData}/**/avatar.{jpg,jpeg,png}`,
+        `${paths.imgRawData}/**/project-*+([^0-9][^w]).{jpg,jpeg,png}`,
+        `${paths.imgRawData}/**/!*_fallback.{jpg,jpeg,png}`,
+    ])
         .pipe(imageResize({ width: defaultWidth }))
-        .pipe(
-            imagemin({
-                plugins: [pngToJpeg({ quality: 100 })],
-            })
-        )
+        .pipe(imagemin({ plugins: [pngToJpeg({ quality: 100 })] }))
         .pipe(imagemin([imagemin.mozjpeg({ quality: 80, progressive: true })]))
         .pipe(rename({ suffix: `_fallback`, extname: '.jpg' }))
-        .pipe(dest('assets/img'));
+        .pipe(dest(paths.imgSrc));
     cb();
 };
 
@@ -100,39 +86,41 @@ const browserSyncReload = (cb) => {
 };
 
 const minifyCss = (cb) => {
-    src('_site/**/*.css')
-        .pipe(cleanCSS())
-        .pipe(dest('_site'));
+    const postcssPlugins = [
+        cssDeclarationSorter({ order: 'concentric-css' }),
+        postcssFlexbugsFixes(),
+        postcssCustomProperties(),
+        postcssCalc(),
+        postcssMergeRules(),
+        cssDeclarationSorter({ order: 'concentric-css' }),
+        autoprefixer(),
+        cssnano(),
+    ];
+
+    src(`${paths.build}/**/*.css`)
+        .pipe(gulpPostcss(postcssPlugins))
+        .pipe(dest(paths.build));
     cb();
 };
 const minifyHtml = (cb) => {
-    src('_site/**/*.html')
+    src(`${paths.build}/**/*.html`)
         .pipe(htmlmin({ collapseWhitespace: true, removeComments: true }))
-        .pipe(dest('_site'));
+        .pipe(dest(paths.build));
     cb();
 };
 const minifyJs = (cb) => {
-    src('_site/**/*.js')
+    src(`${paths.build}/**/*.js`)
         .pipe(
             terser({
                 keep_fnames: true,
                 mangle: false,
             })
         )
-        .pipe(dest('_site'));
+        .pipe(dest(paths.build));
 
     cb();
 };
 
-// const deleteSourceImage = (cb) => {
-//     glob('_site/**/*.{jpg,png}', {}, (err, files) => {
-//         if (err) {
-//             console.error(err);
-//         }
-//         setTimeout(() => Promise.all(files.map((file) => fs.unlink(file))), 2000);
-//     });
-//     cb();
-// };
 // const deleteSourceImage = (cb) => {
 //     (async () => {
 //         const deletedPaths = await del(['_site/assets/img/*.{jpg,png}']);
@@ -172,23 +160,45 @@ const afterBuild = parallel(minifyCss, minifyHtml, minifyJs);
 const build = trueSyncSeries('beforeBuild', 'jekyllBuild', 'afterBuild');
 
 const browserSyncInit = (cb) => {
-    bs.init({
-        server: {
-            baseDir: '_site',
-        },
-    });
+    bs.init({ server: { baseDir: paths.build } });
     cb();
 };
 const watchAll = (cb) => {
-    watch(
-        '.',
-        {
-            ignored: ['_site', 'assets/img', '_data/img_src'],
-            delay: 300,
-            awaitWriteFinish: true,
-        },
-        series(build, browserSyncReload)
-    );
+    const watchOptions = {
+        ignored: [paths.build, paths.imgRawData, paths.imgSrc],
+        delay: 300,
+        awaitWriteFinish: true,
+    };
+    const taskWhenChange = series(build, browserSyncReload);
+
+    watch('.', watchOptions, taskWhenChange);
+    cb();
+};
+
+const minifyCssDev = (cb) => {
+    const postcssPlugins = [
+        cssDeclarationSorter({ order: 'concentric-css' }),
+        postcssFlexbugsFixes(),
+        postcssMergeRules(),
+        cssDeclarationSorter({ order: 'concentric-css' }),
+    ];
+
+    src(`${paths.cssSrc}/**/*.css`)
+        .pipe(gulpPostcss(postcssPlugins))
+        .pipe(dest(paths.cssSrc));
+    cb();
+};
+const minifyScssDev = (cb) => {
+    const postcssPlugins = [
+        cssDeclarationSorter({ order: 'concentric-css' }),
+        postcssFlexbugsFixes(),
+        postcssMergeRules(),
+        cssDeclarationSorter({ order: 'concentric-css' }),
+    ];
+
+    src(`${paths.cssSrc}/**/*.scss`)
+        .pipe(gulpPostcss(postcssPlugins, { parser: postcssScssParser }))
+        .pipe(dest(paths.cssSrc));
     cb();
 };
 
@@ -196,7 +206,10 @@ exports.resizeImage = resizeImage;
 exports.minifyImage = minifyImage;
 exports.minifyImageJPGForFallback = minifyImageJPGForFallback;
 
+exports.minifyStyleDev = series(minifyCssDev, minifyScssDev);
+
 exports.beforeBuild = beforeBuild;
 exports.jekyllBuild = jekyllBuild;
 exports.afterBuild = afterBuild;
+exports.build = build;
 exports.default = series(build, browserSyncInit, watchAll);
